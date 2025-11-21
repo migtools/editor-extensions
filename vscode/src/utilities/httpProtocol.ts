2f1189147233b19a20788fa44e32fa202475c950
Ian Bolton <ibolton@redhat.com>
2025-11-21 18:18:12 -0500

    :ghost: Fix hardcoded extension name for http protocol setting (#1039)
    Signed-off-by: Ian Bolton <ibolton@redhat.com>

--- vscode/src/utilities/httpProtocol.ts
+++ vscode/src/utilities/httpProtocol.ts
@@ -1,6 +1,8 @@
 // HTTP protocol configuration getter
 // Separate file to avoid vscode import in test environments
 
+import { EXTENSION_NAME } from "./constants";
+
 export function getConfigHttpProtocol(): "http1" | "http2" {
   if (process.env.NODE_ENV === "test") {
     return "http1";
@@ -8,6 +10,6 @@ export function getConfigHttpProtocol(): "http1" | "http2" {
 
   // eslint-disable-next-line @typescript-eslint/no-require-imports
   const vscode = require("vscode");
-  const config = vscode.workspace.getConfiguration("konveyor");
+  const config = vscode.workspace.getConfiguration(EXTENSION_NAME);
   return config.get("genai.httpProtocol", "http1");
 }
